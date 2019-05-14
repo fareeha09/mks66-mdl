4ecc74a975d587bdb04621efba03b83fc73dd3cf
fareeha09 <ftabassum@stuy.edu>
2019-05-14 15:37:32 -0400

    basics done

--- script.py
+++ script.py
@@ -51,9 +51,11 @@ def run(filename):
         print command
         if command['op'] == 'push':
             stack.append( [x[:] for x in stack[-1]] )
-        if command['op'] == 'pop':
+            
+        elif command['op'] == 'pop':
             stack.pop()
-        if command['op'] == 'sphere':
+        
+        elif command['op'] == 'sphere':
             add_sphere(tmp, float(command['args'][0]),
                            float(command['args'][1]),
                            float(command['args'][2]),
@@ -61,20 +63,91 @@ def run(filename):
                            step_3d)
             matrix_mult(stack[-1], tmp)
             #if constant variable is present, use those for lighting
-            if command['constants'] != None
-            draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
-        if command['op'] == 'box':
+            if command['constants'] is not None:
+                draw_polygons(tmp, screen, zbuffer, view, ambient,
+                                  light, symbols, reflect)
+            else:
+                draw_polygons(tmp, screen, zbuffer, view, ambient,
+                                  light, symbols, reflect)
+            tmp = []
+            
+        elif command['op'] == 'box':
             add_box(tmp, float(command['args'][0]),
                         float(command['args'][1]),
                         float(command['args'][2]),
                         float(command['args'][3]),
                         float(command['args'][4]),
                         float(command['args'][5]))
-        if command['op'] == 'torus':
-            add_torus(polygons, float(command['args'][0]),
+            matrix_mult(stack[-1], tmp)
+            #if constant variable is present, use those for lighting
+            if command['constants'] is not None:
+                draw_polygons(tmp, screen, zbuffer, view, ambient,
+                                  light, symbols, reflect)
+            else:
+                draw_polygons(tmp, screen, zbuffer, view, ambient,
+                                  light, symbols, reflect)
+            tmp = []
+            
+        elif command['op'] == 'torus':
+            add_torus(tmp, float(command['args'][0]),
                           float(command['args'][1]),
                           float(command['args'][2]),
                           float(command['args'][3]),
                           float(command['args'][4]),
                           step_3d)
+            matrix_mult(stack[-1], tmp)
+            #if constant variable is present, use those for lighting
+            if command['constants'] is not None:
+                draw_polygons(tmp, screen, zbuffer, view, ambient,
+                                  light, symbols, reflect)
+            else:
+                draw_polygons(tmp, screen, zbuffer, view, ambient,
+                                  light, symbols, reflect)
+            tmp = []
+
+        elif command['op'] == 'line':
+            add_edge( tmp, float(command['args'][0]),
+                          float(command['args'][1]),
+                          float(command['args'][2]),
+                          float(command['args'][3]),
+                          float(command['args'][4]),
+                          float(command['args'][5]))
+            matrix_mult(stack[-1], tmp)
+            draw_lines(tmp, screen, zbuffer, color)
+            tmp = []
+
+        elif command['op'] == 'move':
+            t = make_translate(float(command['args'][0]),
+                                   float(command['args'][1]),
+                                   float(command['args'][2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+            
+        elif command['op'] == 'rotate':
+            theta = float(command['args'][1]) * (math.pi / 180)
+            if command['args'][0] == 'x':
+                t = make_rotX(theta)
+            elif command['args'][0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        
+        elif command['op'] == 'scale':
+            t = make_scale(float(command['args'][0]),
+                               float(command['args'][1]),
+                               float(command['args'][2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        
+        elif command['op'] == 'constants':
+            pass
+
+        elif command['op'] == 'display':
+            display(screen)
+            
+        elif command['op'] == 'save':
+            save_extension(screen, command['args'][0])
+        
             
